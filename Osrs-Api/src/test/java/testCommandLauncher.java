5c16a7b355bb6cc2defc9e9e57e61839a40ca125
MakingStan <78844475+MakingStan@users.noreply.github.com>
2022-02-26 18:03:45 +0100

    Delete Osrs-Api directory

--- Osrs-Api/src/test/java/testCommandLauncher.java
+++
@@ -1,9 +0,0 @@
-import api.hiscores.OsrsApi;
-
-public class testCommandLauncher {
-
-    public static void main(String[] args) throws Exception
-    {
-        System.out.println(OsrsApi.RealTimePrices.getlowPrice(821));
-    }
-}
