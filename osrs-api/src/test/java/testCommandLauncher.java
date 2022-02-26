83601408d050e33e8426fdcc9134ea39f0ac8428
MakingStan <78844475+MakingStan@users.noreply.github.com>
2022-02-26 18:02:49 +0100

    Add files via upload
    fixed errors

---
+++ osrs-api/src/test/java/testCommandLauncher.java
@@ -0,0 +1,9 @@
+import api.hiscores.OsrsApi;
+
+public class testCommandLauncher {
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println(OsrsApi.RealTimePrices.getlowPrice(821));
+    }
+}
